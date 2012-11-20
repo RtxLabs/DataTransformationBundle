3ba5c4a25c0938dd1a07528650a1fc1cecf1d5d1
Uwe Klawitter <uwe_klawitter@web.de>
2012-11-20 10:12:21 +0100

    added JeepMock

---
+++ Tests/Mockups/Entity/JeepMock.php
@@ -0,0 +1,15 @@
+<?php
+namespace RtxLabs\DataTransformationBundle\Tests\Mockups\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * RtxLabs\DataTransformationBundle\Tests\Mockups\Entity\JeepMock
+ *
+ * @ORM\Table()
+ * @ORM\Entity()
+ */
+class JeepMock extends CarMock
+{
+
+}
